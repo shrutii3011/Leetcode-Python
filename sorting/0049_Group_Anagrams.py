974782ab013709d7134d15c5caf925b37673d36c
Shruti Bankar <bankarshruti626@gmail.com>
2026-09-01 23:06:20 +0530

    Day 32 : Solve LeetCode #49 - Group Anagrams

---
+++ sorting/0049_Group_Anagrams.py
@@ -0,0 +1,9 @@
+class Solution:
+    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
+        anagrams = defaultdict(list)
+
+        for word in strs:
+            key = ''.join(sorted(word))
+            anagrams[key].append(word)
+
+        return list(anagrams.values())
